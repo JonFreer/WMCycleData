25cb51111e25d65fbdb05333095936c5ea9420cc
Jon Freer <j.freer@hotmail.co.uk>
2023-05-26 15:37:16 +0100

    init

---
+++ api/API/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.openapi.utils import get_openapi
+
+from . import config
+# from .routers import report,crossings
+
+app = FastAPI(root_path="/api",title="CycleCounter")
+app.add_middleware(SessionMiddleware, secret_key=config.SessionSecret)
+# openapi_schema = get_openapi(title="BadlyParked",version="1.0.0",routes=app.routes,description="This is a very custom OpenAPI schema")
+# app.openapi_schema = openapi_schema
+# app.openapi = app.openapi_schema
+# app.include_router(uploads.router)
+# app.include_router(report.router)
+# app.include_router(crossings.router)
---
+++ api/prestart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Running pre start"
+# db-init
+
+uvicorn API.main:app --host 0.0.0.0 --port 80
