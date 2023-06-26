cd0e84eb22eb73e5b6a9281f08e2875a9e0bf39e
George Kaye <georgejkaye@gmail.com>
2023-06-26 21:36:47 +0100

    fix #1: make cards more responsive, fix duplication of stats

--- web/src/pages/counters.tsx
+++ web/src/pages/counters.tsx
@@ -4,7 +4,6 @@ import styles from "../css_modules/counters.module.css";
 import { Counter, CounterPlus } from "../types/types";
 function Counters() {
   const counters = useCounters();
-
   return (
     <div className={styles.main}>
       {counters.map(function (object, i) {
@@ -13,44 +12,48 @@ function Counters() {
     </div>
   );
 }
-
+function StatCard(props: {
+  counter: CounterPlus;
+  count: (c: CounterPlus) => number;
+  description: string;
+}) {
+  return (
+    <a
+      className={`${dashboard_styles.button} ${styles.counterCardButton}`}
+      href={"/counter/" + props.counter.identity}
+    >
+      {props.count(props.counter)} {props.description}
+    </a>
+  );
+}
 function CounterCard({ counter }: { counter: CounterPlus }) {
   return (
     <a
       href={"/counter/" + counter.identity}
-      className={`${dashboard_styles.card} ${styles.cardGap}`}
+      className={`${dashboard_styles.card} ${styles.counterCard} ${styles.cardGap}`}
     >
       <div className={`${dashboard_styles.cardBody} ${styles.cardHolder}`}>
         <div className={styles.cardDetails}>
           <h4 className={styles.cardTitle}>{counter.name}</h4>
           <p>{counter.location_desc}</p>
-          <div className={dashboard_styles.buttonHolder}>
-            <a
-              className={dashboard_styles.button}
-              href={"/counter/" + counter.identity}
-            >
-              {counter.today_count} users today
-            </a>
-            <a
-              className={dashboard_styles.button}
-              href={"/counter/" + counter.identity}
-            >
-              {counter.week_count} users last week
-            </a>
-          </div>
-        </div>
-        <div className={styles.cardContentBG}>
-          <div className={styles.cardContentFiller}></div>
-          <div className={styles.cardContent}>
-            <p className={styles.cardContentTitle}>Users Yesterday</p>
-            <h2 className={styles.cardContentNumber}>
-              {" "}
-              {counter.yesterday_count}
-            </h2>
-          </div>
-          <div className={styles.cardContent}>
-            <p className={styles.cardContentTitle}>Weekly Users</p>
-            <h2 className={styles.cardContentNumber}>{counter.week_count}</h2>
+          <div
+            className={`${dashboard_styles.buttonHolder} ${styles.counterCardButtonHolder}`}
+          >
+            <StatCard
+              counter={counter}
+              count={(c) => c.today_count}
+              description="today"
+            />
+            <StatCard
+              counter={counter}
+              count={(c) => c.yesterday_count}
+              description="yesterday"
+            />
+            <StatCard
+              counter={counter}
+              count={(c) => c.last_week_count}
+              description="past week"
+            />
           </div>
         </div>
       </div>
